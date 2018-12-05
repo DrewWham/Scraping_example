1aade52f2c65c5529e2c97c6fc7245c3a2d63aca
DrewWham <Fcwham@gmail.com>
2018-12-05 09:52:22 -0500

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,3 @@
+install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("XML",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("rvest",repos= "http://lib.stat.cmu.edu/R/CRAN/")
